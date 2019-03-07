8e6ed17b82d754eaffe08b4cb71162b8554423cc
Kevin Quach <kevin.quach@outlook.fr>
2019-03-07 17:09:00 +0100

    Post page in html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,9 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'post', loadChildren: './post/post.module#PostPageModule' }
 ];
 
 @NgModule({
